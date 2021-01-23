dc57d40845d2e1b134b61e28a629e08566b65cb1
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-23 14:21:48 -0500

    fix delete campus front end

--- src/components/views/AllPlayersView.jsx
+++ src/components/views/AllPlayersView.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/style.css';
-import PropTypes from 'prop-types';
 
 const AllPlayersView = props => {
   return (
@@ -8,10 +7,6 @@ const AllPlayersView = props => {
     <h1> Home PAGE</h1>
     </div>
   );
-};
-
-AllPlayersView.propTypes = {
-  allPlayers: PropTypes.array.isRequired
 };
 
 export default AllPlayersView;
--- src/store/utilities/allPlayers.js
+++ src/store/utilities/allPlayers.js
@@ -1,18 +1,11 @@
 import axios from 'axios';
 
 // ACTION TYPES;
-const FETCH_ALL_PLAYERS = "FETCH_ALL_PLAYERS";
 const FETCH_ALL_STUDENTS = "FETCH_ALL_STUDENTS";
 const FETCH_ALL_COMPUSES = "FETCH_ALL_COMPUSES";
 const DELETE_CAMPUS = "DELETE_CAMPUS";
 
 // ACTION CREATORS;
-const fetchAllPlayers = players => {
-  return {
-    type: FETCH_ALL_PLAYERS,
-    payload: players
-  }
-}
 const fetchAllStudents = students => {
   return {
     type: FETCH_ALL_STUDENTS,
@@ -32,24 +25,6 @@ const deleteCampus = id => {
   }
 }
 
-// THUNK CREATORS;
-export const fetchAllPlayersThunk = () => dispatch => {
-  const object = [
-    {
-      id:"1",
-      firstName: "mohammed"
-    },
-    {
-      id:"2",
-      firstName: "karim"
-    }
-  ]
-  return dispatch(fetchAllPlayers(object));/*axios
-    .get('/api/players')
-    .then(res => res.data)
-    .then(players => dispatch(fetchAllPlayers(players)))
-    .catch(err => console.log(err))*/
-}
 export const fetchAllStudentsThunk = () => dispatch => {
   
   return axios
@@ -68,21 +43,20 @@ export const fetchAllCompusesThunk = () => dispatch => {
 export const deleteCampusThunk = id => dispatch => {
   return axios
   .delete(`http://localhost:1234/api/campuses/${id}`)
-  .then(res => res.data)
-  .then(()=> dispatch(deleteCampus))
+  .then(()=> dispatch(deleteCampus(id)))
   .catch(err => console.log(err))
 }
 
 // REDUCER;
 const reducer = (state = [] , action) => {
   switch (action.type) {
-    case FETCH_ALL_PLAYERS:
-      return action.payload;
     case FETCH_ALL_STUDENTS:
       console.log("fetch all students");
       return action.payload;
     case FETCH_ALL_COMPUSES:
       return action.payload;
+      case DELETE_CAMPUS:
+        return state.filter(campus => campus.id != action.payload)
     default:
       return state;
   }
