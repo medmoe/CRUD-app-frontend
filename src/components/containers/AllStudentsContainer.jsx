f2055dc8c2e7799f18ba7cc3e671afcf9b112d60
medmoe <med.seffah@gmail.com>
2021-01-23 18:54:41 -0500

    create a student

--- src/components/containers/AllStudentsContainer.jsx
+++ src/components/containers/AllStudentsContainer.jsx
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchAllStudentsThunk } from '../../thunks';
-import { AllStudentsView } from '../views';
+import { addStudentThunk, fetchAllStudentsThunk } from '../../thunks';
+import { AllStudentsView, AddStudentView } from '../views';
+import { Switch } from 'react-router-dom';
 
 // Smart container;
 class AllStudentsContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      showForm: false
+    }
+    this.addStudent = this.addStudent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    
+  }
   componentDidMount() {
     this.props.fetchAllStudents();
   }
+  //add student
+  addStudent(){
+    this.setState({
+      showForm: true
+    })
+  }
+  //handle form submission
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.addStudent(this.state);
+    this.setState({
+      showForm: false
+    })
+    console.log(window.location);
+    window.location.reload();
+
+  }
+  handleChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
   render() {
-    return <AllStudentsView allStudents={this.props.allPlayers} />
+    if(this.state.showForm){
+      return <AddStudentView handleSubmit = {this.handleSubmit}
+                             handleChange = {this.handleChange} />
+    }else{
+      return <AllStudentsView allStudents={this.props.allPlayers}
+                              addStudent={this.addStudent} />
+    }
   }
 }
-
 // Map state to props;
 const mapState = state => {
   return {
@@ -25,9 +63,10 @@ const mapState = state => {
 // Map dispatch to props;
 const mapDispatch = dispatch => {
   return {
-    fetchAllStudents: () => dispatch(fetchAllStudentsThunk())
+    fetchAllStudents: () => dispatch(fetchAllStudentsThunk()),
+    addStudent: (student) => dispatch(addStudentThunk(student))
+    } 
   }
-}
 
 // Type check props;
 AllStudentsContainer.propTypes = {
--- src/components/views/AllStudentsView.jsx
+++ src/components/views/AllStudentsView.jsx
@@ -6,15 +6,27 @@ import {Link} from 'react-router-dom'
 const AllStudentsView = props => {
   return (
     <div className="all-students">
-       
+      <div>
+        <h1>List of students on campus</h1>
+        <button onClick={props.addStudent}>add students</button>
+      </div>
+      <div className="students-list">
       {props.allStudents.map(student => (
-        <div key={student.id}>
-          <Link to = {`/students/${student.id}`}>
-          <h1>{student.firstName}</h1>
-          <h1>{student.lastName}</h1>
-          </Link>
+        <div key={student.id} className="students-container">
+          <div>
+            <Link to = {`/students/${student.id}`}>
+              <div className="student-image">
+                <img src={student.imageURL} alt="student image"/>
+              </div>
+            </Link>
+            <div className="student-info">
+              <p>Name: {student.firstName}, {student.lastName}</p>
+              <p>Campus: {student.campusId}</p>
+            </div>
+          </div>
         </div>
       ))}
+      </div>
     </div>
   );
 };
