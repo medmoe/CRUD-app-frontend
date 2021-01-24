e3740e9efa2d2c17e45c07fde40695754adafd45
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-23 21:20:32 -0500

    implement delete student again

--- src/components/views/AllStudentsView.jsx
+++ src/components/views/AllStudentsView.jsx
@@ -4,33 +4,39 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom'
 
 
-const AllStudentsView = props => {
-  return (
-    <div className="all-students">
-      <div>
-        <h1>List of students on campus</h1>
-        <button onClick={props.addStudent}>add students</button>
-      </div>
-      <div className="students-list">
-      {props.allStudents.map(student => (
-        <div key={student.id} className="students-container">
-          <div>
-            <Link to = {`/students/${student.id}`}>
-              <div className="student-image">
-                <img src={student.imageURL} alt="student image"/>
+class AllStudentsView extends React.Component{
+  handleDelete = e => {
+    this.props.deleteStudent(e.target.value)
+  }
+  render() {
+    return (
+      <div className="all-students">
+        <div>
+          <h1>List of students on campus</h1>
+          <button onClick={this.props.addStudent}>add students</button>
+        </div>
+        <div className="students-list">
+        {this.props.allStudents.map(student => (
+          <div key={student.id} className="students-container">
+            <div>
+              <Link to = {`/students/${student.id}`}>
+                <div className="student-image">
+                  <img src={student.imageURL} alt="student image"/>
+                </div>
+              </Link>
+              <div className="student-info">
+                <p>Name: {student.firstName}, {student.lastName}</p>
+                <p>Campus: {student.campusId}</p>
               </div>
-            </Link>
-            <div className="student-info">
-              <p>Name: {student.firstName}, {student.lastName}</p>
-              <p>Campus: {student.campusId}</p>
+              <button onClick = {this.handleDelete} value = {student.id}>Delete Student</button>
             </div>
           </div>
+        ))}
         </div>
-      ))}
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 AllStudentsView.propTypes = {
   allStudents: PropTypes.array.isRequired
