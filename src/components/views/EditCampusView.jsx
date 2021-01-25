21606419b5db24404a1a4115816f44ebc669682f
medmoe <med.seffah@gmail.com>
2021-01-24 22:58:41 -0500

    styling update campus view

--- src/components/views/EditCampusView.jsx
+++ src/components/views/EditCampusView.jsx
@@ -1,31 +1,35 @@
 import React, { Component } from 'react';
+import './styles/style.css';
 
 class EditCampusView extends Component{
     render(){
         const students = this.props.students.map(element => {
             return <Student name={element['firstName'].concat(" "+element['lastName'])}  handleCheckBoxChange ={this.props.handleCheckBoxChange} isEnrolled={this.props.isEnrolled}/>
         })
-        return <div>
-            <form onSubmit={this.props.handleSubmit}>
-                <label>name: </label>
-                <input type="text" name="name" value={this.props.name} onChange={this.props.handleChange}></input> <br />
+        return <div className="container updateCampus">
+            <form onSubmit={this.props.handleSubmit} >
+                <div className="form-group">
+                    <label for="nameexample">name: </label>
+                    <input type="text" name="name" value={this.props.name} onChange={this.props.handleChange} className="form-control" id="nameexample"></input> <br />
+                </div>
                 <label>address: </label>
-                <input type="text" name="address" value={this.props.address} onChange={this.props.handleChange}></input> <br />
+                <input type="text" name="address" value={this.props.address} onChange={this.props.handleChange} className="form-control"></input> <br />
                 <label>description: </label>
-                <input type="text" name="description" value={this.props.description} onChange={this.props.handleChange}></input> <br />
+                <input type="text" name="description" value={this.props.description} onChange={this.props.handleChange} className="form-control"></input> <br />
                 <label>imageURL: </label>
-                <input type="text" name="imageURL" value={this.props.imageURL} onChange={this.props.handleChange}></input> <br />
+                <input type="text" name="imageURL" value={this.props.imageURL} onChange={this.props.handleChange} className="form-control"></input> <br />
                 <label>students: </label>
                 {students}
-                <input type="submit"></input>
+                <br />
+                <input type="submit" className="btn btn-primary"></input>
             </form>
         </div>
     }
 }
 export const Student = (props) => {
-    return <div>
-            <p>{props.name}</p>
-            <input type="checkbox" onChange={props.handleCheckBoxChange} checked={props.isEnrolled}></input>
+    return <div className="form-check">
+            <input type="checkbox" onChange={props.handleCheckBoxChange} checked={props.isEnrolled} className="form-check-input"></input>
+            <p className="form-check-label">{props.name}</p>
             </div>
     
 }
