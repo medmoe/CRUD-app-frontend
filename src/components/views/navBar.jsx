d8ef34f0986c5a246c118336225f72630007a69e
medmoe <med.seffah@gmail.com>
2021-01-25 09:25:04 -0500

    styled home page

--- src/components/views/navBar.jsx
+++ src/components/views/navBar.jsx
@@ -6,11 +6,11 @@ class NavBar extends React.Component{
         return (
             <div id="navbar"> 
                 <div id="homelink">
-                    <Link to='/'> Home</Link>
+                    <Link to='/'> <h1 className="display-3">Home</h1></Link>
                 </div>
                 <div className="campus_student_link">
-                    <div id="campuses"><Link to='/campuses' > Campuses </Link></div>
-                    <div id="students"><Link to='/students' > Students </Link></div>
+                    <div id="campuses"><Link to='/campuses' > <h1 className="display-3">Campuses</h1> </Link></div>
+                    <div id="students"><Link to='/students' > <h1 className="display-3">Students</h1> </Link></div>
                 </div>
             </div>
         )
